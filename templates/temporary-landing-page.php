f6e78ad7d9d29e08adb3df950dd63322fa236571
Steffen Muldbjerg <steffenmuldbjerg@gmail.com>
2023-10-27 22:10:00 +0200

    Added temporary landing page

---
+++ templates/temporary-landing-page.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Temporary Landing Page Template
+ * Template Post Type: page
+ *
+ */
+?>
+
+<div class="index">
+
+    <h1>Birgit Marie Studio</h1>
+    <a href="https://www.instagram.com/birgitmariestudio/" target="_blank">@birgitmariestudio</a>
+</div>
+
+<style>
+    body{
+        background-color: #E9E1D6;
+    }
+
+    .index{
+        width: 100%;
+        text-align: center;
+    }
+
+    h1{
+        margin: 10vh 0 40px;
+        font-size: 8vw;
+        font-family: system-ui;
+        text-transform: uppercase;
+        color: #8F6B5D;
+
+    }
+
+    .index a,
+    .index a:visited
+    {
+        font-size: 3vw;
+        font-family: system-ui;
+        text-decoration: none;
+        color: #C88769;
+    }
+</style>
