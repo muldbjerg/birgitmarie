9626f98c42d5ae2e0a7148d3cfaddc02a607f429
Steffen Muldbjerg <steffenmuldbjerg@gmail.com>
2023-11-22 21:48:18 +0100

    Tilføj kontaktoplysninger

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
     </div> <!-- END SIDE-CONTENT -->
     
     <footer>
-        Birgit Marie Studio © <?php echo date("Y") ?>
+        <p>Birgit Marie Studio © <?php echo date("Y") ?></p>
+        <p><a target="_blank" href="mailto:birgitmarieoesterby@gmail.com?body=Hej Birgit Marie">birgitmarieoesterby@gmail.com</a> +45 21 77 87 77</p>
     </footer>
 </div> <!-- END SITE-WRAPPER -->
 
@@ -10,5 +11,33 @@
         padding: 20vw 0 4vw;
         font-size: 1rem;
         color: #A4927C;
+    }
+
+    footer p{
+        margin: 4px 0;
+    }
+
+    footer p:first-of-type{
+        font-weight: 600;
+    }
+
+    footer a, footer a:visited{
+        display: inline-block;
+        margin-right: 16px;
+        color: #A4927C;
+        transition: color 0.1s ease;
+    }
+
+    @media(hover: hover){
+         footer a:hover{
+            color: var(--main-text-color);
+         }
+    }
+
+    
+    @media only screen and (max-width: 960px) {
+        footer{
+            text-align: center;
+        }
     }
 </style>
